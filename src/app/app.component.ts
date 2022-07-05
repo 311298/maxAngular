87983fb09accd6be832a8f8ffc6fa1c7164f7f08
311298 <kashyapanurag109@gmail.com.com>
2022-07-05 13:32:59 +0530

    tracking commit from now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements: any[] = [
-    { type: 'server', name: 'testServer', content: 'this is test!!' }
-  ]
 
-  onServerAdded(serverData: { serverName: string, serverContent: string }) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName, // here "name" is same as input variable type in server-element and "serverName" & "serverContent" is same as output from client component
-      content: serverData.serverContent
-    });
-  }
-
-  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent
-    });
-  }
 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CockpitComponent,
-    ServerElementComponent
   ],
   imports: [
     BrowserModule,
